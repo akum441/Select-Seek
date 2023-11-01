5a21bea06b6c0cb9a65350439a95ac539b7a16b9
Arun Kumar <arun111143@gmail.com>
2023-11-01 14:42:35 +0100

    modified css to make sure button don't inherate the web page styles

--- content.js
+++ content.js
@@ -1,14 +1,17 @@
 let lastSelectedText = "";
 
+function generateUniqueClassName() {
+    return 'uniqueSearchButtonClass' + Date.now();
+}
+
 function createSearchButton(selectedText, x, y) {
-    let button = document.querySelector(".uniqueSearchButtonClass");
+    let button = document.querySelector(".uniqueSearchButtonBase");
+    let uniqueClassName = generateUniqueClassName();
 
     if (!button) {
         button = document.createElement("button");
-        button.className = "uniqueSearchButtonClass";
+        button.className = `uniqueSearchButtonBase ${uniqueClassName}`;
         button.innerHTML = "Search Google";
-        button.style.position = "fixed";
-        button.style.zIndex = "9999";
 
         button.onclick = function () {
             window.open(`https://www.google.com/search?q=${selectedText}`, "_blank");
@@ -18,25 +21,56 @@ function createSearchButton(selectedText, x, y) {
         document.body.appendChild(button);
     }
 
-    // Fix button at the position where the text was selected
-    button.style.top = `${y - 30}px`;
-    button.style.left = `${x + 6}px`;
+    // Create and attach the style element
+    const styleSheet = document.createElement("style");
+    styleSheet.type = "text/css";
+    styleSheet.innerText = `
+        .uniqueSearchButtonBase.${uniqueClassName} {
+            position: absolute !important;
+            font-family: Arial, sans-serif !important;  
+            font-size: 16px !important;                
+            font-weight: normal !important;             
+            font-style: normal !important;            
+            font-variant: normal !important;          
+            text-transform: none !important;        
+            text-decoration: none !important;          
+            letter-spacing: normal !important;       
+            word-spacing: normal !important;           
+            line-height: 1.5 !important;             
+            text-align: center !important;    
+            
+            
+            color: #000 !important;
+            // height:20px !important;
+            zIndex: 9999 !important;
+            backgroundColor: #ffffff !important;
+            border: 1px solid #d4d4d4 !important;
+            color: #000000 !important;
+            padding: 5px !important;
+            cursor: pointer !important;
+            top: ${y - 30}px !important;
+            left: ${x + 6}px !important;
+        }
+    `;
+    document.head.appendChild(styleSheet);
+
+
 }
+
 
 function handleTextSelection(event) {
     let selection = window.getSelection();
-    let selctionToString=selection.toString();
+    let selectionToString = selection.toString();
     let range = selection.rangeCount > 0 ? selection.getRangeAt(0) : null;
-    if (range && !range.collapsed && selctionToString!='') {
+    if (range && !range.collapsed && selectionToString != '') {
         lastSelectedText = selection;
-        createSearchButton(selection, event.clientX, event.clientY);
+        createSearchButton(selection, event.pageX, event.pageY);
     }
 }
 
 document.addEventListener("mouseup", handleTextSelection);
 document.addEventListener("mousedown", function (event) {
-    const button = document.querySelector(".uniqueSearchButtonClass");
-
+    const button = document.querySelector(".uniqueSearchButtonBase");
     if (button && !button.contains(event.target)) {
         button.remove();
     }
